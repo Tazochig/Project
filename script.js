a98cc99c39c39b45c3e3fffe64ac13dda3a0618e
Tazochig <chigladzetazo@gmail.com>
2024-12-21 07:41:07 +0400

    patara update

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-let nomeri = 1;
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+// smoothly ro chavides dacherisas buttonze
+let btn = document.getElementById("choose_us_btn");
+let ctn = document.getElementById("contacts");
+
+btn.addEventListener("click", () => {
+  ctn.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+// videoze custom buttoni
+const video = document.getElementsByClassName("custom-video__video");
+let i;
+for (i = 0; i < video.length; i++) {
+  video[i].addEventListener("click", function () {
+    const controls = this.nextElementSibling;
+    if (controls.innerHTML === "▶") {
+      controls.innerHTML = "| |";
+      this.play();
+    } else {
+      controls.innerHTML = "▶";
+      this.pause();
+    }
+  });
+  video[i].addEventListener("mouseout", function () {
+    const controls = this.nextElementSibling;
+    if (!this.paused) {
+      controls.style.display = "none";
+    }
+  });
+  video[i].addEventListener("mouseover", function () {
+    const controls = this.nextElementSibling;
+    controls.style.display = "flex";
+  });
+  video[i].addEventListener(
+    "ended",
+    function () {
+      const controls = this.nextElementSibling;
+      controls.style.display = "flex";
+      controls.innerHTML = "▶";
+    },
+    false
+  );
 }
